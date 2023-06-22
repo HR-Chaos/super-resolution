362bc37036620e48faf524323f68f9c3d0896b42
dybaleon <102929455+dybaleon@users.noreply.github.com>
2023-06-21 19:01:40 -0700

    Created divide_sets.py
    Add first functional version of divide_sets.py, which splits the HR LR pairs into 60,000 training, 4,000 validation, and 512 testing set pairs.

---
+++ divide_sets.py
@@ -0,0 +1,72 @@
+import os
+import random
+import shutil
+
+
+####################################################################
+#Important: Files remain in the same order among HR LR folder pairs
+####################################################################
+
+# Define the paths - default only applicable after running downscale.ipynb
+hr_images_dir = 'HR_images'
+
+#Modify ci for whatever method is in use
+lr_images_dir = 'LR_images' + '/' + "ci"
+
+hr_train_dir = 'HR_train'
+lr_train_dir = 'LR_train'
+hr_val_dir = 'HR_val'
+lr_val_dir = 'LR_val'
+hr_test_dir = 'HR_test'
+lr_test_dir = 'LR_test'
+
+# Create the destination directories if they don't exist
+os.makedirs(hr_train_dir, exist_ok=True)
+os.makedirs(lr_train_dir, exist_ok=True)
+os.makedirs(hr_val_dir, exist_ok=True)
+os.makedirs(lr_val_dir, exist_ok=True)
+os.makedirs(hr_test_dir, exist_ok=True)
+os.makedirs(lr_test_dir, exist_ok=True)
+
+# Get a list of all HR image files in the HR_images directory
+hr_image_files = [f for f in os.listdir(hr_images_dir) if os.path.isfile(os.path.join(hr_images_dir, f))]
+
+# Shuffle the HR image files
+random.shuffle(hr_image_files)
+
+# Move HR and LR image pairs to the corresponding directories
+train_files = hr_image_files[:60000]
+
+for file_name in train_files:
+    hr_src_path = os.path.join(hr_images_dir, file_name)
+    hr_dst_path = os.path.join(hr_train_dir, file_name)
+    lr_file_name = f"downscaled_ci_{file_name}"
+    lr_src_path = os.path.join(lr_images_dir, lr_file_name)
+    lr_dst_path = os.path.join(lr_train_dir, lr_file_name)
+
+    shutil.move(hr_src_path, hr_dst_path)
+    shutil.move(lr_src_path, lr_dst_path)
+
+val_files = hr_image_files[60000:64000]
+
+for file_name in val_files:
+    hr_src_path = os.path.join(hr_images_dir, file_name)
+    hr_dst_path = os.path.join(hr_val_dir, file_name)
+    lr_file_name = f"downscaled_ci_{file_name}"
+    lr_src_path = os.path.join(lr_images_dir, lr_file_name)
+    lr_dst_path = os.path.join(lr_val_dir, lr_file_name)
+
+    shutil.move(hr_src_path, hr_dst_path)
+    shutil.move(lr_src_path, lr_dst_path)
+
+for file_name in hr_image_files[64000:]:
+    hr_src_path = os.path.join(hr_images_dir, file_name)
+    hr_dst_path = os.path.join(hr_test_dir, file_name)
+    lr_file_name = f"downscaled_ci_{file_name}"
+    lr_src_path = os.path.join(lr_images_dir, lr_file_name)
+    lr_dst_path = os.path.join(lr_test_dir, lr_file_name)
+
+    shutil.move(hr_src_path, hr_dst_path)
+    shutil.move(lr_src_path, lr_dst_path)
+
+print("Files moved successfully!")
